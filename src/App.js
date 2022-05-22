2c254a4c3b6ffc6d1dd4aa8b7afac63d069d3682
m-nakamine <kawarabasami.dev@gmail.com>
2022-05-22 16:03:11 +0900

    add dispatch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
 import './App.css';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import Count from './components/count'
 
 function App() {
   const count = useSelector((state) => state.countReducer.count);
+
+  const dispatch = useDispatch();
+  const increase = () => {
+    dispatch({type: 'INCREASE_COUNT'});
+  }
+  const decrease = () => {
+    dispatch({type: 'DECREASE_COUNT'});
+  }
+  const addArray = () => {
+    dispatch({type: 'ADD_ARRAY'});
+  }
+
   return (
     <div>
       <h1>Redux Learn</h1>
       <p>Count:{count}</p>
+      <button onClick={increase}>Up</button>
+      <button onClick={decrease}>Down</button>
+      <button onClick={addArray}>add</button>
       <Count />
     </div>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,26 @@
 import {createStore, combineReducers} from 'redux';
 
-const countReducer = (state = {count: 50}) => {
-    return state
+const countReducer = (state = {count: 50}, action) => {
+    switch (action.type){
+        case 'INCREASE_COUNT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREASE_COUNT':
+            return {
+                count: state.count - 1
+            };
+        default:
+            return state;
+    }
 }
 const postsReducer = (state = {posts: [
     {id: 1, title: 'Reduxについて'},
     {id: 2, title: 'ReduxのHooksについて'}
-]}) => {
+]}, action) => {
+    if (action.type === 'ADD_ARRAY'){
+        return {posts: [...state.posts, {id: 3, title: 'test'}]}
+    }
     return state
 }
 
